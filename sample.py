66dac7f970f3b0fedb438076f4516a3a91f5258f
Ulfur_Orn <ulfurinn@gmail.com>
2020-03-20 08:59:05 +0000

    Add now supports single numbers

--- sample.py
+++ sample.py
@@ -3,3 +3,6 @@
 def add(numbers):
     if numbers == "":
         return 0
+
+    else:
+        return int(numbers)
--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,10 @@
 from sample import add
 
+
 def test_empty_string():
     assert add("") == 0
+
+
+def test_single_num():
+    assert add("1") == 1
+    assert add("83") == 83
